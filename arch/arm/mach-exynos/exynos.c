e09160df6a3c9d87e3ce64a22341b3af5deaf1cb
Thomas Abraham <thomas.ab@samsung.com>
2014-07-28 13:35:50 +0530

    ARM: Exynos: switch to using generic cpufreq driver for Exynos4210/5250/5420
    The new CPU clock type allows the use of generic CPUfreq drivers. So for
    Exynos4210/5250, switch to using generic cpufreq driver. For Exynos5420,
    which did not have CPUfreq driver support, enable the use of generic
    CPUfreq driver.
    
    Suggested-by: Tomasz Figa <t.figa@samsung.com>
    Cc: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Thomas Abraham <thomas.ab@samsung.com>
    Reviewed-by: Tomasz Figa <t.figa@samsung.com>

--- arch/arm/mach-exynos/exynos.c
+++ arch/arm/mach-exynos/exynos.c
@@ -281,6 +281,28 @@ static void __init exynos_init_irq(void)
 	exynos_map_pmu();
 }
 
+static const struct of_device_id exynos_cpufreq_matches[] = {
+	{ .compatible = "samsung,exynos5420", .data = "arm-bL-cpufreq-dt" },
+	{ .compatible = "samsung,exynos5250", .data = "cpufreq-cpu0" },
+	{ .compatible = "samsung,exynos4210", .data = "cpufreq-cpu0" },
+	{ .compatible = "samsung,exynos5440", .data = "exynos5440-cpufreq" },
+	{ /* sentinel */ }
+};
+
+static void __init exynos_cpufreq_init(void)
+{
+	struct device_node *root = of_find_node_by_path("/");
+	const struct of_device_id *match;
+
+	match = of_match_node(exynos_cpufreq_matches, root);
+	if (!match) {
+		platform_device_register_simple("exynos-cpufreq", -1, NULL, 0);
+		return;
+	}
+
+	platform_device_register_simple(match->data, -1, NULL, 0);
+}
+
 static void __init exynos_dt_machine_init(void)
 {
 	struct device_node *i2c_np;
@@ -320,7 +342,7 @@ static void __init exynos_dt_machine_init(void)
 			of_machine_is_compatible("samsung,exynos5250"))
 		platform_device_register(&exynos_cpuidle);
 
-	platform_device_register_simple("exynos-cpufreq", -1, NULL, 0);
+	exynos_cpufreq_init();
 
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 }
