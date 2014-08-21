08ead417add94c5bb5cc2d266cc661d0778c0750
Vikas Sajjan <vikas.sajjan@samsung.com>
2014-08-21 18:52:37 +0545

    POSTED: ARM: exynos: Add PMU and S2R support for exynos5800 SoC
    Adds PMU and S2R support for exynos5800 SoC.
    
    Signed-off-by: Vikas Sajjan <vikas.sajjan@samsung.com>

--- arch/arm/mach-exynos/pmu.c
+++ arch/arm/mach-exynos/pmu.c
@@ -726,6 +726,19 @@ static void exynos5420_pmu_init(void)
 	pr_info("EXYNOS5420 PMU initialized\n");
 }
 
+static void exynos5800_pmu_init(void)
+{
+	unsigned int value;
+
+	exynos5420_pmu_init();
+
+	value = pmu_raw_readl(EXYNOS5420_LPI_MASK);
+	value |= EXYNOS5800_POWER_GATE_CTRL;
+	pmu_raw_writel(value, EXYNOS5420_LPI_MASK);
+
+	pr_info("EXYNOS5800 PMU initialized\n");
+}
+
 
 static const struct exynos_pmu_data exynos4210_pmu_data = {
 	.pmu_config	= exynos4210_pmu_config,
@@ -752,6 +765,12 @@ static struct exynos_pmu_data exynos5420_pmu_data = {
 	.powerdown_conf	= exynos5420_powerdown_conf,
 };
 
+static struct exynos_pmu_data exynos5800_pmu_data = {
+	.pmu_config	= exynos5420_pmu_config,
+	.pmu_init	= exynos5800_pmu_init,
+	.powerdown_conf	= exynos5420_powerdown_conf,
+};
+
 /*
  * PMU platform driver and devicetree bindings.
  */
@@ -771,6 +790,9 @@ static const struct of_device_id exynos_pmu_of_device_ids[] = {
 	}, {
 		.compatible = "samsung,exynos5420-pmu",
 		.data = &exynos5420_pmu_data,
+	}, {
+		.compatible = "samsung,exynos5800-pmu",
+		.data = &exynos5800_pmu_data,
 	},
 	{ /*sentinel*/ },
 };
