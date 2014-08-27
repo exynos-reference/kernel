9a1913bed4726f63023556b2d36e92fcaa809c5d
Ajay Kumar <ajaykumar.rs@samsung.com>
2014-08-27 19:59:37 +0530

    POSTED: drm/bridge: Add helper functions for drm_bridge
    A set of helper functions are defined in this patch to make
    bridge driver probe independent of the drm flow.
    
    The bridge devices register themselves on a lookup table
    when they get probed by calling "drm_bridge_add".
    
    The parent encoder driver waits till the bridge is available
    in the lookup table(by calling "of_drm_find_bridge") and then
    continues with its initialization.
    
    The encoder driver should also call "drm_bridge_attach" to pass
    on the drm_device, encoder pointers to the bridge object.
    
    drm_bridge_attach inturn calls drm_bridge_init to register itself
    with the drm core. Later, it calls "bridge->funcs->attach" so that
    bridge can continue with other initializations.
    
    Signed-off-by: Ajay Kumar <ajaykumar.rs@samsung.com>

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -1028,7 +1028,7 @@ int drm_bridge_init(struct drm_device *dev, struct drm_bridge *bridge)
 	if (ret)
 		goto out;
 
-	bridge->dev = dev;
+	bridge->drm = dev;
 
 	list_add_tail(&bridge->head, &dev->mode_config.bridge_list);
 	dev->mode_config.num_bridge++;
@@ -1047,7 +1047,7 @@ EXPORT_SYMBOL(drm_bridge_init);
  */
 void drm_bridge_cleanup(struct drm_bridge *bridge)
 {
-	struct drm_device *dev = bridge->dev;
+	struct drm_device *dev = bridge->drm;
 
 	drm_modeset_lock_all(dev);
 	drm_mode_object_put(dev, &bridge->base);
